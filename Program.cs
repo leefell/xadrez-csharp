db04cb36f724b107b5e817ae1e5ace3e8a7e55a3
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-23 15:12:35 -0300

    Classe PosicaoXadrez criada

--- Program.cs
+++ Program.cs
@@ -7,22 +7,11 @@ namespace xandrezConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 1));
+            Console.WriteLine(pos);
 
-                Tela.imprimirTabuleiro(tab);
-
-                Console.ReadLine();
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosicao());
 
             Console.ReadLine();
         }
---
+++ xadrez/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+
+    // Essa classe é pra auxiliar a pensar como o xadrez normal
+
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
