ee304b5fce1fa4b75a9032a265808fbcfe6d4d6f
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-23 09:53:14 -0300

    Tratando erros com exceptions personalizadas

--- Program.cs
+++ Program.cs
@@ -7,17 +7,24 @@ namespace xandrezConsole
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 1));
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tela.imprimirTabuleiro(tab);
 
-            Tela.imprimirTabuleiro(tab);
+                Console.ReadLine();
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
-
         }
     }
 }
---
+++ Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+﻿namespace tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string messagem) : base(messagem)
+        {
+
+        }
+    }
+}
