1c175ce8936aa084270c5b3e2241d19471badd24
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-23 08:22:05 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xandrezConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
