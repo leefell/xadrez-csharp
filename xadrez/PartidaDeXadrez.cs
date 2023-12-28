c183cc1bc0f11f0a49e9be1279050df268be9e1f
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-27 21:11:02 -0300

    Finalizado o jogo de xadrez

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino); // peça movida é armazenada na variavel
+
+            // # jogada especial promocao (peao ascendido para dama)
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -158,8 +173,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino); // peça movida é armazenada na variavel
 
             // #jogada especial EnPassant
             if (p is Peao && (destino.linha == origem.linha - 2) || (destino.linha == origem.linha + 2))
